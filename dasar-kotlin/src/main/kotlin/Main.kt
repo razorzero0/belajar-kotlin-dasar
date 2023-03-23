319d90de0946291eebb2b5a55bd258a9e18b39d3
muhainun059@gmail.com <muhainun059@gmail.com>
2023-03-23 14:19:34 +0700

    tes

---
+++ dasar-kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    //val = const, var = let
+val a:Int = 5;
+var b= 5;
+    b+=1;
+    println(a + b);
+
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+
+    val arr = listOf("yayan","rido");
+    for(i in arr){
+        println(i)
+    }
+
+}
